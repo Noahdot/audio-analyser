1eb9c8f76e703bf25bbc8d5a0906f80dcf236c5e
noahdot <mayor20xx@gmail.com>
2024-11-14 23:46:53 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
-  <div>
-    <button @click="start">Audio analyzer</button>
+  <div class="wrapper">
+    {{ showNumber }}
+    <div class="controll-bar">
+      <button @click="start">Audio analyzer</button>
+      <button @click="add">+</button>
+      <button @click="min">-</button>
+    </div>
+    <div class="audio-wrapper">
+      <div v-for="(item, index) in dataArray" :key="index" :style="{ height: `${item}px` }"></div>
+    </div>
+    <br>
+    {{ dataArray }}
   </div>
 </template>
 <script setup lang="ts">
@@ -8,6 +18,10 @@
 import { ref } from 'vue';
 const source = ref<MediaStreamAudioSourceNode>();
 const analyzer = ref<AnalyserNode>();
+const oscillator = ref<OscillatorNode>();
+// const dataArray = ref<Uint8Array>();
+const dataArray = ref<number[]>();
+const showNumber = ref(0);
 
 const start  = () => {
   navigator.mediaDevices.getUserMedia({ audio: true })
@@ -15,7 +29,9 @@ const start  = () => {
       const audioContext = new AudioContext();
       source.value = audioContext.createMediaStreamSource(stream);
       analyzer.value = audioContext.createAnalyser();
+      oscillator.value = audioContext.createOscillator();
 
+      setupOscillator();
       run();
     })
     .catch((err) => {
@@ -24,16 +40,61 @@ const start  = () => {
 }
 
 const run = () => {
-  const dataArray = new Uint8Array(analyzer.value.frequencyBinCount);
+  analyser.smoothingTimeConstant
+  const uint8Array = new Uint8Array(analyzer.value.frequencyBinCount);
+
+  // source.value?.connect(analyzer.value);
+
+  oscillator.value.start();
+  oscillator.value?.connect(analyzer.value);
   const update = () => {
-    analyzer.value.getByteFrequencyData(dataArray);
-    console.log(dataArray);
+    analyzer.value.getByteFrequencyData(uint8Array);
+    dataArray.value = Array.from(uint8Array);
     requestAnimationFrame(update);
   }
 
   requestAnimationFrame(update);
 }
 
+const add = () => {
+  showNumber.value = oscillator.value.frequency.value ++;
+}
+
+const min = () => {
+  showNumber.value = oscillator.value.frequency.value --;
+}
+
+const setupOscillator = () => {
+  if (!oscillator.value) return;
+
+  oscillator.value.type = 'sine';
+  oscillator.value.frequency.value = 20;
+}
+
+// oscillator.stop();
+
 </script>
 <style lang="scss" scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .control-bar {
+
+  }
+
+  .audio-wrapper {
+    display: flex;
+    align-items: flex-end;
+    width: 1024px;
+    height: 255px;
+    background-color: #333;
+
+    & > div {
+      width: 1px;
+      background-color: #fff;
+    }
+  }
+}
 </style>
