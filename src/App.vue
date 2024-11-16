b8a54f2e5f3f2b14d7dac7bccb2bc32f7e069863
noahdot <mayor20xx@gmail.com>
2024-11-16 23:07:47 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,31 @@
 <template>
   <div class="wrapper">
-    {{ showNumber + ` - maxNumbr: ${maxNumber} - maxIndex: ${maxIndex}` }}
+      {{ sex }}
     <div class="controll-bar">
       <button @click="start">start</button>
-      <button @click="add">+</button>
-      <button @click="min">-</button>
-      <!-- <button @click="test(1)">1</button>
-      <button @click="test(2)">2</button>
-      <button @click="test(3)">3</button> -->
-      <input type="range" min="0" max="400" step="10" v-model="showNumber" />
       <button @click="procAnalyser">Audio analyser</button>
     </div>
-    <div class="audio-wrapper">
-      <div v-for="(item, index) in dataArray" :key="index" :style="{ height: `${item}px` }"></div>
+    <div class="audio-wrapper" :class="{ active }">
+      <div
+        v-for="(item, index) in showDataArray"
+        :key="index"
+        :style="{ 
+          height: `${255 - item * 10}px`
+        }"
+      ></div>
     </div>
-    <br>
-    {{ dataArray }}
   </div>
 </template>
 <script setup lang="ts">
-// import HelloWorld from './components/HelloWorld.vue'
-import { ref, watch } from 'vue';
+import { ref } from 'vue';
 const source = ref<MediaStreamAudioSourceNode>();
 const analyser = ref<AnalyserNode>();
 const oscillator = ref<OscillatorNode>();
-// const dataArray = ref<Uint8Array>();
 const dataArray = ref<number[]>();
-const showNumber = ref(0);
+const showDataArray = ref<number[]>();
+const active = ref(false);
+const sex = ref('');
 
-const test = (num) => {
-  oscillator.value.frequency.value = 100 + num * 10;
-}
 
 const start  = () => {
   navigator.mediaDevices.getUserMedia({ audio: true })
@@ -40,7 +35,6 @@ const start  = () => {
       analyser.value = audioContext.createAnalyser();
       oscillator.value = audioContext.createOscillator();
 
-      setupOscillator();
       run();
     })
     .catch((err) => {
@@ -52,31 +46,32 @@ const maxNumber = ref(0);
 const maxIndex = ref(0);
 
 const run = () => {
-  // analyser.value.smoothingTimeConstant = 0;
+  active.value = true;
+  sex.value = '';
   // const uint8Array = new Uint8Array(analyser.value.frequencyBinCount);
   const uint8Array = new Uint8Array(24);
 
   source.value?.connect(analyser.value);
   // oscillator.value?.connect(analyser.value);
 
-  oscillator.value.start();
   analyserData.value.length = 0;
 
   const update = () => {
     maxNumber.value = 0;
     analyser.value.getByteFrequencyData(uint8Array);
     dataArray.value = Array.from(uint8Array);
+    showDataArray.value = dataArray.value.map(item => Math.round(item / 10));
     // dataArray.value = Array.from(uint8Array).filter(item => item > 100);
 
     
     for (let i = 0; i < dataArray.value.length; i++) {
-      if (dataArray.value[i] > maxNumber.value) {
+      if (dataArray.value[i] >= maxNumber.value) {
         maxNumber.value = dataArray.value[i];
         maxIndex.value = i;
       }
     }
     if (maxNumber.value > 150) analyserData.value.push(maxIndex.value);
-    // 3 開始是有效資料，5-7 男生 8以上女生
+    // 5-7 男生 8以上女生
     // 整段錄音過程要把這個 maxIndex 記錄到一個陣列，然後把陣列中50%相似的值取出來依個數做平均，這就是平均音頻
 
 
@@ -86,29 +81,11 @@ const run = () => {
   requestAnimationFrame(update);
 }
 
-const add = () => {
-  showNumber.value = oscillator.value.frequency.value ++;
-}
-
-const min = () => {
-  showNumber.value = oscillator.value.frequency.value --;
-}
-
-watch(showNumber, (v) => {
-  oscillator.value.frequency.value = v;
-});
-
-const setupOscillator = () => {
-  if (!oscillator.value) return;
-
-  oscillator.value.type = 'sine';
-  oscillator.value.frequency.value = 80;
-}
-
 const result = ref({});
 const analyserData = ref([]);
 const procAnalyser = () => {
   result.value = {};
+  active.value = false;
   // analyserData.value.length = 0;
   source.value?.disconnect(analyser.value);
   // console.log(analyserData.value);
@@ -121,16 +98,22 @@ const procAnalyser = () => {
     }
   });
 
-  console.log(result.value);
-  const max = Math.max(...Object.values(result.value));
+  // console.log(result.value);
+  // const max = Math.max(...Object.values(result.value));
   // console.log(max);
-  const maxKey = Object.keys(result.value).reduce((preKey, curKey) => {
-    return result.value[preKey] > result.value[curKey]? preKey: curKey;
-  });
-  console.log(maxKey);
+  const keys = Object.keys(result.value);
+  if (keys.length === 0) return console.log('no voice data');
+  
+  const entries = Object.entries(result.value);
+  entries.sort((a, b) => b[1] - a[1]);
+  const sortKey = entries.map(item => item[0]);
+  console.log(sortKey);
+  sex.value = sortKey[0] < 8? 'Man': 'Woman';
+  // const maxKey = Object.keys(result.value).reduce((preKey, curKey) => {
+  //   return result.value[preKey] > result.value[curKey]? preKey: curKey;
+  // });
+  // console.log(maxKey);
 }
-
-// oscillator.stop();
 
 </script>
 <style lang="scss" scoped>
@@ -138,21 +121,25 @@ const procAnalyser = () => {
   display: flex;
   flex-direction: column;
   align-items: center;
-
-  .control-bar {
-
-  }
+  justify-content: center;
+  width: 100%;
 
   .audio-wrapper {
     display: flex;
-    align-items: flex-end;
-    width: 1024px;
+    align-items: flex-start;
+    width: 90%;
+    max-width: 1024px;
     height: 255px;
     background-color: #333;
+    
+    &.active {
+      background: linear-gradient(to top, rgb(175, 255, 175) 0%, rgb(255, 255, 157) 30%, rgb(253, 128, 128) 100%);
+    }
 
     & > div {
-      width: 42px;
-      background-color: #599fdb;
+      width: calc(100% / 24);
+      height: 255px;
+      background-color: #333;
     }
   }
 }
