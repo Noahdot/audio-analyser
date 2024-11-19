87d4fd1a411543b375e36c975385205fccf06b9f
noahdot <mayor20xx@gmail.com>
2024-11-19 23:53:54 +0800

    feat: i18n init

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue';
+import { createI18n } from 'vue-i18n';
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const i18n = createI18n({});
+const app = createApp(App);
+app.use(i18n);
+app.mount('#app')
