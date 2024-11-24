1ca3d9e4715dd56bba7a4ef6dd76838e7f1507b3
noahdot <mayor20xx@gmail.com>
2024-11-24 23:37:47 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+  plugins: [vue()],
+  base: '/audio-analyser/'
 });
