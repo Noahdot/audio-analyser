37e2faf852c07963e3249a964a83649d24984ce4
noahdot <mayor20xx@gmail.com>
2024-11-26 22:26:23 +0800

    chore(vite.config.ts): update base config to use env variable
    Modified the `base` config in `vite.config.ts` to dynamically use `env.VITE_BASE_URL` instead of a
    hardcoded string.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  base: '/audio-analyser/'
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [vue()],
+    base: env.VITE_BASE_URL,
+  }
+});
